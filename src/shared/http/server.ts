8a2802f952c6bfbfcd2bab32e635fec2310b811f
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-09-10 13:42:12 -0300

    chore: add dotenv and config routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,14 +1,13 @@
-import express, { Request, Response } from "express";
+import "dotenv/config";
+import express from "express";
 import "express-async-errors";
 import cors from "cors";
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
 
-app.get("/", (req: Request, res: Response) => {
-  return res.json({ message: "Hello World!" });
-});
-
-app.listen("3333", () => console.log("Server is running!"));
+app.listen(process.env.PORT, () => console.log("Server is running!"));

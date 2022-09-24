cbfb1f6b4d33c62e336f17e9872125d553e7eda3
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-09-24 16:16:33 -0300

    feat:  create database conection

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,27 +1,12 @@
+import { dataSource } from "@shared/typeorm";
 import "dotenv/config";
 import "reflect-metadata";
-import express, { NextFunction, Request, Response } from "express";
-import "express-async-errors";
-import swaggerUi from "swagger-ui-express";
-import cors from "cors";
-import { routes } from "./routes";
-import { AppError } from "@shared/errors/AppError";
-import swaggerFile from "../../swagger.json";
 
-const app = express();
+import { app } from "./app";
 
-app.use(cors());
-app.use(express.json());
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(routes);
-
-// Middleware de erros da aplicação
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({ status: "error", message: error.message });
-  }
-
-  return res.status(500).json({ status: "error", message: "Internal server error!" });
-});
-
-app.listen(process.env.PORT, () => console.log("Server is running!"));
+dataSource
+  .initialize()
+  .then(() => {
+    app.listen(process.env.PORT, () => console.log("Server is running!"));
+  })
+  .catch(err => console.log("Error initializing server", err));

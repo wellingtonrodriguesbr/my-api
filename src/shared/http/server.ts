95c35ca1547720f6e17144031a13988767cc5c7f
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-09-24 10:43:30 -0300

    chore: add typeorm and sqlite3

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";

9f7c61eee1f3ade7f21ed9e1e74af9682c745e50
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-07 07:23:55 -0300

    feat: create users table on database

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { Role } from "@roles/entities/Role";
 import { DataSource } from "typeorm";
 import { CreateRolesTable1664103295889 } from "./migrations/1664103295889-CreateRolesTable";
+import { CreateUsersTable1665137880643 } from "./migrations/1665137880643-CreateUsersTable";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
   entities: [Role],
-  migrations: [CreateRolesTable1664103295889],
+  migrations: [CreateRolesTable1664103295889, CreateUsersTable1665137880643],
 });

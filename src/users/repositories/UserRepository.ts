9f699791da83b8ebb3d542c7b30f3b7fe29a6538
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-08 13:26:15 -0300

    feat: create User repository

---
+++ src/users/repositories/UserRepository.ts
@@ -0,0 +1,54 @@
+import { dataSource } from "@shared/typeorm";
+import { User } from "@users/entities/User";
+import { Repository } from "typeorm";
+import { CreateUsersDTO, IUsersRepository, PaginateParams, UsersPaginateProperties } from "./IUsersRepository";
+
+export class UserRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = dataSource.getRepository(User);
+  }
+
+  async create({ name, email, password, isAdmin, role }: CreateUsersDTO): Promise<User> {
+    const user = this.repository.create({ name, email, password, isAdmin, role });
+    return this.repository.save(user);
+  }
+
+  async save(user: User): Promise<User> {
+    return this.repository.save(user);
+  }
+
+  async delete(user: User): Promise<void> {
+    await this.repository.remove(user);
+  }
+
+  async findByName(name: string): Promise<User | null> {
+    return this.repository.findOneBy({ name });
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.repository.findOneBy({ email });
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return this.repository.findOneBy({ id });
+  }
+
+  async findAll({ page, skip, take }: PaginateParams): Promise<UsersPaginateProperties> {
+    const [users, count] = await this.repository
+      .createQueryBuilder("r")
+      .leftJoinAndSelect("r.role", "role")
+      .skip(skip)
+      .take(take)
+      .getManyAndCount();
+    const result = {
+      per_page: take,
+      total: count,
+      current_page: page,
+      data: users,
+    };
+
+    return result;
+  }
+}

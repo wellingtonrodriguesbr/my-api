754322a14db27efbc13e40e3573fc89bd8780dbd
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-08 12:55:01 -0300

    feat: create entity User

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,4 +1,5 @@
 import { Role } from "@roles/entities/Role";
+import { User } from "@users/entities/User";
 import { DataSource } from "typeorm";
 import { CreateRolesTable1664103295889 } from "./migrations/1664103295889-CreateRolesTable";
 import { CreateUsersTable1665137880643 } from "./migrations/1665137880643-CreateUsersTable";
@@ -7,6 +8,6 @@ import { AddRoleIdToUsersTable1665138325079 } from "./migrations/1665138325079-A
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
-  entities: [Role],
+  entities: [Role, User],
   migrations: [CreateRolesTable1664103295889, CreateUsersTable1665137880643, AddRoleIdToUsersTable1665138325079],
 });

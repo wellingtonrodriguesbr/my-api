a43cb4566819d74791f8a267d1b70d08473ed9f8
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-08 14:27:07 -0300

    feat: create user routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { rolesRouter } from "@roles/http/routes/roles.routes";
+import { usersRouter } from "@users/http/routes/users.routes";
 import { Router } from "express";
 
 const routes = Router();
 
 routes.use("/roles", rolesRouter);
+routes.use("/users", usersRouter);
 
 export { routes };
---
+++ src/users/http/routes/users.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { celebrate, Joi, Segments } from "celebrate";
+import { container } from "tsyringe";
+import { CreateUserController } from "@users/useCases/createUser/CreateUserController";
+
+export const usersRouter = Router();
+const createUserController = container.resolve(CreateUserController);
+
+usersRouter.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required,
+      email: Joi.string().email().required,
+      password: Joi.string().required,
+      isAdmin: Joi.boolean().required,
+      roleId: Joi.string().uuid().required,
+    },
+  }),
+  (req, res) => {
+    return createUserController.handle(req, res);
+  },
+);

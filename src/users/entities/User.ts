df37f11ad8b35a576642f258bf0be0108f4d5449
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-08 13:05:17 -0300

    feat: relation role table with user table

--- src/users/entities/User.ts
+++ src/users/entities/User.ts
@@ -1,4 +1,5 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { Role } from "@roles/entities/Role";
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
 @Entity("users")
@@ -20,6 +21,11 @@ export class User {
 
   @Column()
   avatar?: string;
+
+  @ManyToOne(() => Role, {
+    cascade: true,
+  })
+  role: Role;
 
   @CreateDateColumn()
   created_at: Date;

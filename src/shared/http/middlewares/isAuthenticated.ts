6b50d81f9429bf9d79fc0bc565be28241fd977ed
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-09 09:57:37 -0300

    feat: created middleware to  private routes

---
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -0,0 +1,21 @@
+import { AppError } from "@shared/errors/AppError";
+import { NextFunction, Request, Response } from "express";
+import { Secret, verify } from "jsonwebtoken";
+import authConfig from "@config/auth";
+
+export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Failed to verify access token", 401);
+  }
+
+  const token = authHeader.replace("Bearer ", "");
+
+  try {
+    verify(token, authConfig.jwt.secret as Secret);
+    return next();
+  } catch (err) {
+    throw new AppError("Invalid authentication token", 401);
+  }
+};

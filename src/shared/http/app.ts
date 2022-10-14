7596331e15ecda3d49c529860659c1f4f847c52e
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-14 06:25:45 -0300

    chore: create static route

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -6,12 +6,14 @@ import { errors } from "celebrate";
 import { routes } from "./routes";
 import { AppError } from "@shared/errors/AppError";
 import swaggerFile from "../../swagger.json";
+import uploadConfig from "@config/upload";
 import "@shared/container";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(uploadConfig.directory));
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(routes);
 app.use(errors());
--- src/users/entities/User.ts
+++ src/users/entities/User.ts
@@ -1,7 +1,7 @@
 import { Role } from "@roles/entities/Role";
 import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
-import { Exclude } from "class-transformer";
+import { Exclude, Expose } from "class-transformer";
 
 @Entity("users")
 export class User {
@@ -31,6 +31,14 @@ export class User {
 
   @CreateDateColumn()
   created_at: Date;
+
+  @Expose({ name: "avatar_url" })
+  getAvatarUrl(): string | null {
+    if (!this.avatar) {
+      return null;
+    }
+    return `${process.env.AVATAR_URL}/${this.avatar}`;
+  }
 
   constructor() {
     if (!this.id) {

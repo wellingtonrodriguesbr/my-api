c8a44264e9544374f3bc49cd010ee7fcf9b2b2de
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-14 06:37:32 -0300

    feat: create showProfile use case and controller

---
+++ src/users/useCases/showProfile/ShowProfileController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { instanceToInstance } from "class-transformer";
+import { ShowProfileUseCase } from "./ShowProfileUseCase";
+
+export class CreateUserController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const showProfileUseCase = container.resolve(ShowProfileUseCase);
+
+    const userId = req.user.id;
+    const user = await showProfileUseCase.execute({ userId });
+
+    return res.json(instanceToInstance(user));
+  }
+}

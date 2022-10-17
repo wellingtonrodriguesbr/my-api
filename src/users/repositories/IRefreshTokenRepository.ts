3b1dbc49c93bf611516d92de3815909e5e56ce56
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-17 05:54:19 -0300

    feat: create refresh token repository interface

---
+++ src/users/repositories/IRefreshTokenRepository.ts
@@ -0,0 +1,14 @@
+import { RefreshToken } from "@users/entities/RefreshToken";
+
+type CreateRefreskTokenDTO = {
+  user_id: string;
+  token: string;
+  expires: Date;
+  valid: boolean;
+};
+
+export interface IRefreshTokenRepository {
+  create({ expires, token, user_id, valid }: CreateRefreskTokenDTO): Promise<RefreshToken>;
+  findByToken(token: string): Promise<RefreshToken | null>;
+  invalidate(refresh_token: RefreshToken): Promise<void>;
+}

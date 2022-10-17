c9a4776c0836a2f64bd199eb3742dbc1453f3fce
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-17 05:30:31 -0300

    chore: create refresh token table

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -4,10 +4,16 @@ import { DataSource } from "typeorm";
 import { CreateRolesTable1664103295889 } from "./migrations/1664103295889-CreateRolesTable";
 import { CreateUsersTable1665137880643 } from "./migrations/1665137880643-CreateUsersTable";
 import { AddRoleIdToUsersTable1665138325079 } from "./migrations/1665138325079-AddRoleIdToUsersTable";
+import { CreateRefreshTokensTable1665994923583 } from "./migrations/1665994923583-CreateRefreshTokensTable";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
   entities: [Role, User],
-  migrations: [CreateRolesTable1664103295889, CreateUsersTable1665137880643, AddRoleIdToUsersTable1665138325079],
+  migrations: [
+    CreateRolesTable1664103295889,
+    CreateUsersTable1665137880643,
+    AddRoleIdToUsersTable1665138325079,
+    CreateRefreshTokensTable1665994923583,
+  ],
 });

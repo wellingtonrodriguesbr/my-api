c023ea71daa205549854a4d60815e982f325329b
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-18 05:40:57 -0300

    feat: create middleware to user info

---
+++ src/users/http/middlewares/addUserInfoToRequest.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { decode } from "jsonwebtoken";
+
+type JwtPayloadProps = {
+  sub: string;
+};
+
+export const addUserInfoToRequest = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: true, code: "token.invalid", message: "Access token not present." });
+  }
+
+  const token = authHeader.replace("Bearer ", "");
+
+  if (!token) {
+    return res.status(401).json({ error: true, code: "token.invalid", message: "Access token not present." });
+  }
+
+  try {
+    const decodedToken = decode(token);
+    const { sub } = decodedToken as JwtPayloadProps;
+    req.user = { id: sub };
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: true, code: "token.invalid", message: "Access token not present." });
+  }
+};
